a5a69f017127cf171cc070453174b48fc48984de
Yuanjing Zhu <110933007+YZhu0225@users.noreply.github.com>
2022-10-07 03:38:33 +0000

    demo

--- main.sh
+++ main.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
+# generate random password
 
 if [ $# -eq 1 ]; then
-        length=$1
+    length=$1
 else
-        length=8
+    length=12
 fi
-digits=({2..9})
-lower=({a..k} {m..n} {p..z})
-upper=({A..N} {P..Z})
-CharArray=(${digits[*]} ${lower[*]} ${upper[*]})
-ArrayLength=${#CharArray[*]}
+lowercase=({a..k} {m..n} {p..z})        # l looks like 1, o looks like 0, so we skip them
+uppercase=({A..N} {P..Z})               # O looks like 0, so we skip it
+number=({0..9})
+combination=(${number[*]} ${lowercase[*]} ${uppercase[*]})
+clength=${#combination[*]}
 password=""
 for i in `seq 1 $length`
 do
-        index=$(($RANDOM%$ArrayLength))
-        char=${CharArray[$index]}
-        password=${password}${char}
+    index=$(($RANDOM%$clength))
+    value=${combination[$index]}
+    password=${password}${value}
 done
 echo $password
-
